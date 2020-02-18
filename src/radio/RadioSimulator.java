47f1e08ded227639be48e826786ed60fbef54cfc
BrarKhush2000 <59895850+BrarKhush2000@users.noreply.github.com>
2020-02-18 10:21:47 -0600

    Update RadioSimulator.java

--- src/radio/RadioSimulator.java
+++ src/radio/RadioSimulator.java
@@ -15,6 +15,7 @@ public class RadioSimulator {
     
     public static void main(String[] args)
     {
+        //Midterm is today
         //watch it
         //God keeps my head above the water
         
